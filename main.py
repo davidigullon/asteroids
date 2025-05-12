e7320fafe5dc728ee68a75e0c397abb3013f284d
davidigullon <davidigullon@gmail.com>
2025-05-12 12:03:38 +0100

    reset after messing up project. at the stage of mkaing groups

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
 import pygame
-
 from constants import *
-from player import *
-from circleshape import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
 
-
-
-
-    running = True
-    while running:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-        
-        player.update(dt)
-        screen.fill((0,0,0))
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+                return
 
-    pygame.quit()
-    
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
