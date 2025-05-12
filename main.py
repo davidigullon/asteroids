3d946e9101268870dfd0cf99bb3c73b113ba7e63
davidigullon <davidigullon@gmail.com>
2025-05-12 08:55:31 +0100

    added rotation to player object

--- main.py
+++ main.py
@@ -4,29 +4,31 @@ from constants import *
 from player import *
 from circleshape import *
 
-
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-dt = 0
-x = SCREEN_WIDTH / 2
-y = SCREEN_HEIGHT / 2
-player = Player(x, y)
-
-
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill((0,0,0))
-    player.draw(screen)
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
 
-pygame.quit()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        player.update(dt)
+        screen.fill((0,0,0))
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
     
 
 
